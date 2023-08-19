17bc699e4439c0e864c0bea80139108ed989a9e9
LucelhoCristiano <lucelhocristiano28@gmail.com>
2023-08-18 22:13:28 -0300

    :tada: corrigindo bug no JSn

--- Link_Three/script.js
+++ Link_Three/script.js
@@ -5,11 +5,11 @@ function toggleMode() {
   const img = document.querySelector('#profile img');
 
   if (html.classList.contains('light')) {
-    img.src = '../assets/img/avatar-light.png';
+    img.src = './assets/img/avatar-light.png';
     img.alt = 'Avatar light';
   }
   else {
-    img.src = '../assets/img/avatar.png';
+    img.src = './assets/img/avatar.png';
     img.alt = 'Avatar';
   }
 
